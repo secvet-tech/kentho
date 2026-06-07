84d2f0c9ce7dc9392e4fc0b50941b93e657cff25
_kentho <kenneththomsennmbk@gmail.com>
2026-06-07 09:46:17 -0700

    added some functionality to receive correspondence

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 window.alert('welcome to my page!');
+var mailtag = document.getElementById('button1');
+mailtag.addEventListener('click', () => {
+window.location.href = 'mailto:kenneththomsennmbk@gmail.com';
+})
